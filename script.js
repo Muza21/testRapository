9589bfddd17ad09ad6028427622b12c6c8a66962
Muza21 <lasha.muzashvili1@gmail.com>
2022-07-11 10:47:44 +0400

    change in html js and css

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const panels = document.querySelectorAll('.panel')
 
 panels.forEach((panel) => {
@@ -12,19 +11,4 @@ function removeActiveClasses(){
     panels.forEach((panel) => {
         panel.classList.remove('active')
     })
-=======
-const panels = document.querySelectorAll('.panel')
-
-panels.forEach((panel) => {
-    panel.addEventListener('click',() =>{
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
-
-function removeActiveClasses(){
-    panels.forEach((panel) => {
-        panel.classList.remove('active')
-    })
->>>>>>> 4066c4165e78cbc91e7e66e4567d72fde173e71f
 }
